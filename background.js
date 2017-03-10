76c39389010574df1c25b3fbfff033e53d657580
parag00991 <parag00991@gmail.com>
2017-03-10 14:43:33 +0530

    final commit

--- background.js
+++ background.js
@@ -22,6 +22,6 @@ function clickHandler(data,tab) {
       chrome.windows.create({url: "https://twitter.com/intent/tweet?url="+encodeURIComponent(data.srcUrl),type: "panel"});
       break;
       case "page":
-      chrome.windows.create({url: "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title),type: "panel"});
+      chrome.windows.create({url: "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+(data.pageUrl),type: "panel"});
     }
 }
