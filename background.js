b9da67a28a429a45e2c966196fda19ae80df518a
parag00991 <parag00991@gmail.com>
2017-03-10 14:18:25 +0530

    add dynamic options- selection/link/image/page

--- background.js
+++ background.js
@@ -1,11 +1,27 @@
-chrome.contextMenus.create({
-  title: "Twitter-Social-Toolkit",
-  contexts: ["selection"],  //It is used so that this option in context menu only displays when the user selects some text
-  onclick: myFunction
-});
+var contextsList = ["selection", "link", "image", "page"];
 
-function myFunction(selectedText) {
+for(i=0;i<contextsList.length;i++){
+var context=contextsList[i];
+var titleX="Twitter Toolkit: Share this "+context+" on your twitter profile";
+chrome.contextMenus.create({title: titleX,contexts: [context],  //It is used so that this option in context menu only displays when the user selects some text
+  onclick: myFunction, id:context
+});
+}
+
+function myFunction(data) {
     //alert('You just clicked me folk!');
     //alert(selectedText.selectionText);
-    chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+selectedText.selectionText});
+    switch (data.menuItemId) {
+      case "selection":
+      chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+data.selectionText});
+      break;
+      case "link":
+      chrome.tabs.create({url: "https://twitter.com/intent/tweet?url="+data.linkUrl});
+      break;
+      case "image":
+      chrome.tabs.create({url: "https://twitter.com/intent/tweet?url="+data.srcUrl});
+      break;
+      case "page":
+      chrome.tabs.create({url: "https://twitter.com/intent/tweet?text=MyPage"});
+    }
 }
