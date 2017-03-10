77ac6ab121ff1ae3b35040e7f3c46561681d25e0
parag00991 <parag00991@gmail.com>
2017-03-10 13:35:33 +0530

    add extension option in context menu on selection

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
-alert();
+chrome.contextMenus.create({
+  title: "Twitter-Social-Toolkit",
+  contexts: ["selection"],  //It is used so that this option in context menu only displays when the user selects some text
+  onclick: myFunction
+});
+
+function myFunction() {
+    alert('You just clicked me folk!');
+}
